320a8c9b668742a37dbde54b392de30ea8eac76e
rscheiwe <rscheiwe@gmail.com>
2018-06-01 14:02:10 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by do |word|
+    word.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
+
 end
