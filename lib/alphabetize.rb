34cd32a32b695f4e7d7a60f6a8b1c591f94cf5e4
rscheiwe <rscheiwe@gmail.com>
2018-06-01 14:19:18 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,12 +2,15 @@ require 'pry'
 
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
+ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+
 def alphabetize(arr)
   # code here
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+
   arr.sort_by do |word|
-    word.split("").map do |letter|
-      ESPERANTO_ALPHABET.index(letter)
-    end
+    
+    word.tr(ESPERANTO_ALPHABET, ascii)
   end
 
 end
